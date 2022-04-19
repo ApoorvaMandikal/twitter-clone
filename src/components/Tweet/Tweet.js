3da6cdc38139a3c0f80f187d645ef58fbca74726
Apoorva Mandikal <apoorvagirish76@gmail.com>
2022-04-19 12:44:02 +0530

    Compose tweet and display on home page logic.

--- src/components/Tweet/Tweet.js
+++ src/components/Tweet/Tweet.js
@@ -4,40 +4,57 @@ import {BsEmojiSmile} from 'react-icons/fa'
 import {GrGallery} from 'react-icons/gr'
 import {AiOutlineFileGif} from 'react-icons/ai'
 import './tweet.css'
+import { authentication } from '../../Firebase/firebase';
 
-const Tweet = (props) => {
+const Tweet = ({show, onClose}) => {  
 
-  const {user, timestamp, onTweet} = props;
     const [text, setText] = useState('')
+
 
     const handleChange = (e) => {
       setText(e.target.value)
     }
     
-    const onSubmit = (e)=>{
-      e.preventDefault()
-      onTweet({text})
+    const onClickTweet = async (e)=>{
+      try{
+        const body = {
+          "tweet": text,
+          "email": authentication.currentUser.email,
+          "timestamp": new Date().toISOString(),
+        }
+      const res = await fetch('http://localhost:5000/tweet', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(body)
+      })
+      onClose()
+      
       setText('')
-      //console.log(text);
-      //timeStamp: 545986.5
+    }
+    catch(e){
+      alert('Failed to add, please try again later')
+      console.log(e);
+    }
     }
    
   return (
-    <div className={`modal ${props.show ? 'show' : ''}`} onClick={props.onClose}>
+    <div className={`modal ${show ? 'show' : ''}`} onClick={onClose}>
         <div className="modal-content" onClick={e => e.stopPropagation()}>
         <div className='modal-d'>
           <div className="modal-header">
-          <FaTimes onClick={props.onClose} className="close-button"/>
+          <FaTimes onClick={onClose} className="close-button"/>
           </div>
           <div className="modal-body">
-          <input type='text' 
+          <textarea 
             placeholder='Whats Happening?' 
             value={text} 
             onChange = {handleChange}
           />
           </div>
           <div className="modal-footer">
-            <button onClick={onSubmit} className="submit-button">
+            <button onClick={onClickTweet} className="submit-button">
               Tweet
             </button>
           </div>

fff2415d5fa83bd63ee676a18ba228cd22b6bd96
Apoorva Mandikal <apoorvagirish76@gmail.com>
2022-04-20 18:06:15 +0530

    Right Sidebar properly aligned.

--- src/components/Right-Sidebar/RightSidebar.jsx
+++ src/components/Right-Sidebar/RightSidebar.jsx
@@ -15,27 +15,40 @@ const RightSidebar = () => {
           <div className='topics-trending'>
             <div className='trending'>
               <div className='genre-trending'>Music</div>
+              <div>&nbsp; &bull; &nbsp; </div>
               <div className='word-trending'>Trending</div>
             </div>
             <div className='topic-trending'><h2>Taylor Swift</h2></div>
-            <div className='no-tweets'>62.3K Tweets</div>
+            <div className='no-tweets'>12.3K Tweets</div>
           </div>
 
           <div className='topics-trending'>
             <div className='trending'>
-              <div className='genre-trending'>Music</div>
+              <div className='genre-trending'>Art & Culture</div>
+              <div>&nbsp; &bull; &nbsp; </div>
               <div className='word-trending'>Trending</div>
             </div>
-            <div className='topic-trending'><h2>Taylor Swift</h2></div>
-            <div className='no-tweets'>62.3K Tweets</div>
+            <div className='topic-trending'><h2>Episode 55</h2></div>
+            <div className='no-tweets'>6300 Tweets</div>
           </div>
 
           <div className='topics-trending'>
             <div className='trending'>
-              <div className='genre-trending'>Music</div>
+              <div className='genre-trending'>Health</div>
+              <div>&nbsp; &bull; &nbsp; </div>
+              <div className='word-trending'>Live</div>
+            </div>
+            <div className='topic-trending'><h2>Covid-19 new variant</h2></div>
+            <div className='no-tweets'>729 Tweets</div>
+          </div>
+
+          <div className='topics-trending'>
+            <div className='trending'>
+              <div className='genre-trending'>War</div>
+              <div>&nbsp; &bull; &nbsp; </div>
               <div className='word-trending'>Trending</div>
             </div>
-            <div className='topic-trending'><h2>Taylor Swift</h2></div>
+            <div className='topic-trending'><h2>Ukraine Strikes back!</h2></div>
             <div className='no-tweets'>62.3K Tweets</div>
           </div>
         </div>

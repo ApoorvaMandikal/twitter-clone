155c86f4ccacb9b3479e80a7692a5c68b934e917
Apoorva Mandikal <apoorvagirish76@gmail.com>
2022-04-21 11:22:04 +0530

    Clean working of the modal and layout changes.

--- src/components/Tweet/Tweet.js
+++ src/components/Tweet/Tweet.js
@@ -1,27 +1,28 @@
 import { useState } from 'react'
-import {FaTimes} from 'react-icons/fa'
-import {BsEmojiSmile} from 'react-icons/fa'
-import {GrGallery} from 'react-icons/gr'
-import {AiOutlineFileGif} from 'react-icons/ai'
+import { FaTimes } from 'react-icons/fa'
+import { BsEmojiSmile } from 'react-icons/bs'
+import { AiOutlineFileGif } from 'react-icons/ai'
+import { RiGalleryFill } from 'react-icons/ri'
+import { CgProfile } from 'react-icons/cg'
 import './tweet.css'
 import { authentication } from '../../Firebase/firebase';
 
-const Tweet = ({show, onClose, setIsLoadingTweet}) => {  
+const Tweet = ({ show, onClose, setIsLoadingTweet }) => {
 
-    const [text, setText] = useState('')
+  const [text, setText] = useState('')
 
-    const handleChange = (e) => {
-      setText(e.target.value)
-    }
-    
-    const onClickTweet = async (e)=>{
-      try{
-        const body = {
-          "tweet": text,
-          "email": authentication.currentUser.email,
-          "timestamp": new Date().toISOString(),
-        }
-        setIsLoadingTweet(true);
+  const handleChange = (e) => {
+    setText(e.target.value)
+  }
+
+  const onClickTweet = async (e) => {
+    try {
+      const body = {
+        "tweet": text,
+        "email": authentication.currentUser.email,
+        "timestamp": new Date().toISOString(),
+      }
+      setIsLoadingTweet(true);
       const res = await fetch('http://localhost:5000/tweet', {
         method: 'POST',
         headers: {
@@ -34,39 +35,54 @@ const Tweet = ({show, onClose, setIsLoadingTweet}) => {
       setIsLoadingTweet(false);
 
       onClose()
-      
+
       setText('')
-      
-      
+
+
     }
-    catch(e){
+    catch (e) {
       alert('Failed to add, please try again later')
       console.log(e);
     }
-    }
-   
+  }
+
   return (
     <div className={`modal ${show ? 'show' : ''}`} onClick={onClose}>
-        <div className="modal-content" onClick={e => e.stopPropagation()}>
+      <div className="modal-content" onClick={e => e.stopPropagation()}>
         <div className='modal-d'>
           <div className="modal-header">
-          <FaTimes onClick={onClose} className="close-button"/>
+            <FaTimes onClick={onClose} className="modal-close-button" />
           </div>
-          <div className="modal-body">
-          <textarea 
-            placeholder='Whats Happening?' 
-            value={text} 
-            onChange = {handleChange}
-          />
+          <div className='modal-body'>
+            <div className='modal-profile-picture'>
+              <CgProfile className='modal-picture' style={{ color: '#50b7f5' }} />
+            </div>
+            <div className="modal-text-footer">
+              <div className='modal-text'>
+                <textarea
+                  className='modal-textarea'
+                  placeholder='Whats Happening?'
+                  value={text}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="modal-footer">
+                <div className='additions'>
+                  <div><RiGalleryFill className='modal-additional-icons' style={{ color: '#50b7f5' }} /> </div>
+                  <div><BsEmojiSmile className='modal-additional-icons' style={{ color: '#50b7f5' }} /></div>
+                  <div><AiOutlineFileGif className='modal-additional-icons' style={{ color: '#50b7f5' }} /></div>
+                </div>
+                <div className='tweet-button-modal' >
+                  <button onClick={onClickTweet} className="submit-button">
+                    Tweet
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="modal-footer">
-            <button onClick={onClickTweet} className="submit-button">
-              Tweet
-            </button>
-          </div>
-        </div>
         </div>
       </div>
+    </div>
   )
 }
 

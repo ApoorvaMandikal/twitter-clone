de0884d82dcb4d1f12e6349b9fc5e8e7cc60e851
Apoorva Mandikal <apoorvagirish76@gmail.com>
2022-04-25 08:16:31 +0530

    Removed some unnecessary comments.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import Sidebar from './components/Sidebar/Sidebar';
-import { useEffect, useState } from 'react';
 import { authentication } from './Firebase/firebase';
 import {useAuthState} from 'react-firebase-hooks/auth';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthenticationPage from './components/Authentication/AuthenticationPage';
-import Post from './components/Posts/Post';
-import Home from './components/HomePage/Home';
 import DisplayTweet from './components/DisplayTweetPage/DisplayTweet';
 import ProtectedRoute from './ProtectedRoute';
 
@@ -16,10 +12,6 @@ import ProtectedRoute from './ProtectedRoute';
 
 function App() {
   const [user] = useAuthState(authentication);
-
-    // <div>    <DisplayTweet/> </div>
-
-
 
   return (
   <Router>

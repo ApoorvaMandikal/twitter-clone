dd4643a65bb99efb63f39298e5c07a4ab7e70457
Apoorva Mandikal <apoorvagirish76@gmail.com>
2024-04-26 02:08:25 +0530

    Added static posts to display anytime the app opens instead of it being empty

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -1,109 +1,116 @@
-import { useState, useEffect } from "react"
-import { authentication } from "../../Firebase/firebase"
-import Tweet from "../Tweet/Tweet"
-import Sidebar from "../Sidebar/Sidebar"
-import Tweets from "../Tweets/Tweets"
-import MobileSidebar from "../Mobile-Sidebar/MobileSidebar"
-import "./home.css"
-import { CgProfile } from 'react-icons/cg'
+import { useState, useEffect } from "react";
+import { authentication } from "../../Firebase/firebase";
+import Tweet from "../Tweet/Tweet";
+import Sidebar from "../Sidebar/Sidebar";
+import Tweets from "../Tweets/Tweets";
+import MobileSidebar from "../Mobile-Sidebar/MobileSidebar";
+import "./home.css";
+import { CgProfile } from "react-icons/cg";
 import { FaPlus } from "react-icons/fa";
-import RightSidebar from "../Right-Sidebar/RightSidebar"
-import { Container, Button, Link, lightColors, darkColors } from 'react-floating-action-button'
-
-
+import RightSidebar from "../Right-Sidebar/RightSidebar";
+import StaticPosts from "../Posts/StaticPosts";
+import {
+  Container,
+  Button,
+  Link,
+  lightColors,
+  darkColors,
+} from "react-floating-action-button";
 
 const Home = () => {
-
   const [show, setShow] = useState(false);
 
-  const [showResults, setShowResults] = useState(false)
+  const [showResults, setShowResults] = useState(false);
 
-  const [isLoadingTweet, setIsLoadingTweet] = useState(false)
+  const [isLoadingTweet, setIsLoadingTweet] = useState(false);
 
-  const onClick  =()=>{
-    setShowResults(wasOpened => !wasOpened);
-  }
+  const onClick = () => {
+    setShowResults((wasOpened) => !wasOpened);
+  };
 
   const [open, setOpen] = useState(false);
 
+  const [posts, setPosts] = useState([]);
 
-    const [posts, setPosts] = useState([])
+  useEffect(() => {
+    const getPosts = async () => {
+      const res = await fetch("http://localhost:5000/tweet");
+      const data = await res.json();
+      const postFromServer = await fetchPosts();
+      setPosts(postFromServer);
+    };
+    getPosts();
+  }, [isLoadingTweet]);
 
-    useEffect(()=> {
-      const getPosts = async() => {
-        const res = await fetch('http://localhost:5000/tweet')
-        const data = await res.json()  
-        const postFromServer = await fetchPosts()
-        setPosts(postFromServer)
+  //Fetch Posts
+  const fetchPosts = async () => {
+    const res = await fetch("http://localhost:5000/tweet");
+    const data = await res.json();
 
-      }
-      getPosts()
-      
-    },[isLoadingTweet])
-  
-    //Fetch Posts
-    const fetchPosts = async () => {
-      const res = await fetch('http://localhost:5000/tweet')
-      const data = await res.json()
-  
-      return data
-    }
+    return data;
+  };
 
-    //Delete Post
-    const deletePost = async (id) => {
-      await fetch(`http://localhost:5000/tweet/${id}`, {
-        method: 'DELETE'
-      })
-      console.log(id)
-      setPosts(posts.filter((post)=> post.id!==id))
-    }
+  //Delete Post
+  const deletePost = async (id) => {
+    await fetch(`http://localhost:5000/tweet/${id}`, {
+      method: "DELETE",
+    });
+    console.log(id);
+    setPosts(posts.filter((post) => post.id !== id));
+  };
 
-
-  const str = authentication.currentUser.displayName
+  const str = authentication.currentUser.displayName;
 
   return (
-      <div className="home-container">
-        <Sidebar className="sidebar" id="sidebar" setIsLoadingTweet={setIsLoadingTweet}/>
-        
+    <div className="home-container">
+      <Sidebar
+        className="sidebar"
+        id="sidebar"
+        setIsLoadingTweet={setIsLoadingTweet}
+      />
 
-            <div className="posts-container">
-              <div className="welcome">
-                <div className="mobile-home-logo">
-                  <div className="sidebar-button">
-                    <CgProfile className='sidebar-profile-picture' style={{ color: '#50b7f5'}} onClick={()=>setOpen(true)}/>
-                    <MobileSidebar onClose={()=> setOpen(false)} open={open}/>
-                  </div>
-                  <div><a href="/">Home</a></div>
-                </div>
-                <div className="welcome-greeting">
-                 Welcome, &nbsp;
-                 {
-                     str.substring(0, str.indexOf(' '))
-                 }
-                </div>
-              </div>
-                <Tweets className="post" posts={posts} onDelete={deletePost}/>
-                <Container className="floating-tweetB">
-              <Button
-                className = "fab"
-                tooltip="Tweet!"
-                // rotate={true}
-                onClick={() => setShow(true)} 
-                styles={{backgroundColor: '#50b7f5'}}>
-                  <FaPlus style={{height: '2em', width: '2em', color: '#ffffff'}}/>
-                </Button>
-            </Container>
-            <Tweet onClose={() => setShow(false)} show={show} />
+      <div className="posts-container">
+        <div className="welcome">
+          <div className="mobile-home-logo">
+            <div className="sidebar-button">
+              <CgProfile
+                className="sidebar-profile-picture"
+                style={{ color: "#50b7f5" }}
+                onClick={() => setOpen(true)}
+              />
+              <MobileSidebar onClose={() => setOpen(false)} open={open} />
             </div>
-            
-            
-          <RightSidebar className="rightSidebar"/>
-          {/* <Fab id="fab" color="secondary" aria-label="edit">
+            <div>
+              <a href="/">Home</a>
+            </div>
+          </div>
+          <div className="welcome-greeting">
+            Welcome, &nbsp;
+            {str.substring(0, str.indexOf(" "))}
+          </div>
+        </div>
+        <Tweets className="post" posts={posts} onDelete={deletePost} />
+        <Container className="floating-tweetB">
+          <Button
+            className="fab"
+            tooltip="Tweet!"
+            // rotate={true}
+            onClick={() => setShow(true)}
+            styles={{ backgroundColor: "#50b7f5" }}
+          >
+            <FaPlus style={{ height: "2em", width: "2em", color: "#ffffff" }} />
+          </Button>
+        </Container>
+        <Tweet onClose={() => setShow(false)} show={show} />
+        <StaticPosts />
+      </div>
+
+      <RightSidebar className="rightSidebar" />
+      {/* <Fab id="fab" color="secondary" aria-label="edit">
               <EditIcon />
             </Fab> */}
-        </div>  
-        
-  )
-}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
---
+++ src/components/Posts/StaticPosts.jsx
@@ -0,0 +1,101 @@
+import Sidebar from "../Sidebar/Sidebar";
+import "./posts.css";
+import { CgProfile } from "react-icons/cg";
+import { GrGallery } from "react-icons/gr";
+import { AiOutlineFileGif, AiOutlineRetweet } from "react-icons/ai";
+import { FiHeart } from "react-icons/fi";
+import { BsChat } from "react-icons/bs";
+import { authentication } from "../../Firebase/firebase";
+import { useState } from "react";
+import { useLocation, useNavigate, Link } from "react-router-dom";
+import Tweet from "../Tweet/Tweet";
+import { FaTimes } from "react-icons/fa";
+import TimeAgo from "react-timeago";
+import ReactTimeAgo from "react-time-ago";
+
+//import {IoChatbubbleOutline} from 'react-icons/io5'
+
+const Post = ({ post, onDelete }) => {
+  const user = authentication.currentUser;
+  let navigate = useNavigate();
+  let location = useLocation();
+
+  const commentPage = () => {
+    navigate(`/status/${post.id}`, { state: { post_details: post } });
+    navigate(`/status/${post.timestamp}`, { state: { post_details: post } });
+  };
+
+  return (
+    <>
+      <div className="display-post">
+        <div className="container-postpage">
+          <div className="post-profile-picture">
+            <CgProfile className="post-picture" style={{ color: "#50b7f5" }} />
+          </div>
+
+          <div className="post-content">
+            <div className="first-line">
+              <div className="user-dets">
+                <div className="user-name">Apoorva</div>
+                <div className="user-handle" style={{ opacity: 0.5 }}>
+                  @apoorva@faketwitter.com
+                </div>
+                <div className="dot">&bull;</div>
+                <div className="date-time">Feb 20</div>
+              </div>
+            </div>
+
+            <div className="content">
+              <div className="wordings">
+                Welcome to Twitter Clone - Tweeter!
+              </div>
+              {/* <div className="tweet-functionalities">
+                <BsChat className="icons" onClick={commentPage} />
+                <FiHeart className="icons" />
+                <AiOutlineRetweet className="icons" />
+              </div> */}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="display-post">
+        <div className="container-postpage">
+          <div className="post-profile-picture">
+            <CgProfile className="post-picture" style={{ color: "#50b7f5" }} />
+          </div>
+
+          <div className="post-content">
+            <div className="first-line">
+              <div className="user-dets">
+                <div className="user-name">Apoorva</div>
+                <div className="user-handle" style={{ opacity: 0.5 }}>
+                  @apoorva@faketwitter.com
+                </div>
+                <div className="dot">&bull;</div>
+                <div className="date-time">
+                  {/* <ReactTimeAgo date={post.timestamp} timeStyle="twitter" /> */}
+                  Feb 20
+                </div>
+              </div>
+            </div>
+
+            <div className="content">
+              <div className="wordings">
+                For now the app only supports text tweets & comments, but stay
+                tuned for upgrades! Thanks for visiting this site :)
+              </div>
+              {/* <div className="tweet-functionalities">
+                <BsChat className="icons" onClick={commentPage} />
+                <FiHeart className="icons" />
+                <AiOutlineRetweet className="icons" />
+              </div> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Post;
